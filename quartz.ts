18b0703fb24e39968f0cfb3643065421c3f20732
PSR Charan <charansairam153@gmail.com>
2026-06-25 19:43:55 +0530

    Inject linguistic notation transformer directly from quartz entrypoint

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,8 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import LinguisticNotation from "./plugins/linguistic-notation/index.js"
 
 const config = await loadQuartzConfig()
+config.plugins.transformers.unshift(LinguisticNotation())
+
 export default config
 export const layout = await loadQuartzLayout()
